8f58079b3dc2d82b059bde1bea1b3493ac79b4ef
logangutknecht <105251737+logangutknecht@users.noreply.github.com>
2022-12-02 13:53:47 -0800

    firebase edits

---
+++ site/redirect.js
@@ -0,0 +1,7 @@
+<script>
+    // also we need to make the O small
+    setTimeout(() => {
+        window.location.href="https://youtu.be/dQw4w9WgXcQ";    
+    }, 4000);
+    // 4000 = 4 seconds ( 1000 = 1 second)
+</script>
